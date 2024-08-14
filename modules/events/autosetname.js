952ffa6916e2f0c558f280d7f38fc81896b13699
YIN4U <moussabenalisalah@gmail.com>
2024-08-14 11:50:21 +0100

    Update autosetname.js

--- modules/events/autosetname.js
+++ modules/events/autosetname.js
@@ -12,7 +12,7 @@ var memJoin = event.logMessageData.addedParticipants.map(info => info.userFbId)
 	for (let idUser of memJoin) {
 		const { readFileSync, writeFileSync } = global.nodemodule["fs-extra"];
 		const { join } = global.nodemodule["path"]
-		const pathData = join("./modules/commands","cache", "autosetname.json");
+		const pathData = join("./modules/commands","autosetname.json");
 		var dataJson = JSON.parse(readFileSync(pathData, "utf-8"));
 		var thisThread = dataJson.find(item => item.threadID == threadID) || { threadID, nameUser: [] };
 		if (thisThread.nameUser.length == 0) return 
